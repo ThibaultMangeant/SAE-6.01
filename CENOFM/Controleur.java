cad0aa855674afd3d83f2e1a177e1dc552bc17c5
Thibault Mangeant <166180018+ThibaultMangeant@users.noreply.github.com>
2026-02-10 23:12:22 +0100

    Graphique termine
    Pour l'instant je créer moi même une solution dans le contrôleur mais elle s'affiche parfaitement, plus qu'à me donner la vraie solution

--- CENOFM/Controleur.java
+++ CENOFM/Controleur.java
@@ -1,10 +1,13 @@
 package CENOFM;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import CENOFM.IHM.FrameMain;
 import CENOFM.IHM.FrameGraphique;
 import CENOFM.metier.LectureVrp;
+import CENOFM.metier.Noeud;
 import CENOFM.metier.ConversionVrpDat;
 import CENOFM.metier.RecuitSimuleCVRP;
 import CENOFM.metier.DonneesVrp;
@@ -25,7 +28,36 @@ public class Controleur {
 
 	public void extractionDonnee( String txt, int nbV )
 	{
-		try { this.donnee = this.lect.charger(txt, nbV); new FrameGraphique(15, this.donnee.getClients());  } 
+		try
+		{
+			this.donnee = this.lect.charger(txt, nbV);
+
+			List<Noeud> clientsv1 = new ArrayList<Noeud>();
+			clientsv1.add(this.donnee.getDepot());
+			clientsv1.add(new Noeud(2, 0, 5, 0));
+			clientsv1.add(new Noeud(4, 0, 2, 0));
+			clientsv1.add(new Noeud(3, 3, 5, 0));
+			clientsv1.add(new Noeud(10, 3, 2, 0));
+
+			List<Noeud> clientsv2 = new ArrayList<Noeud>();
+			clientsv2.add(this.donnee.getDepot());
+			clientsv2.add(new Noeud(5, 0, 0, 0));
+			clientsv2.add(new Noeud(6, 0, 0, 0));
+			clientsv2.add(new Noeud(8, 0, 0, 0));
+
+			List<Noeud> clientsv3 = new ArrayList<Noeud>();
+			clientsv3.add(this.donnee.getDepot());
+			clientsv3.add(new Noeud(1, 0, 0, 0));
+			clientsv3.add(new Noeud(7, 0, 0, 0));
+			clientsv3.add(new Noeud(9, 0, 0, 0));
+
+			List<List<Noeud>> solution = new ArrayList<>();
+			solution.add(clientsv1);
+			solution.add(clientsv2);
+			solution.add(clientsv3);
+
+			new FrameGraphique(solution);
+		}
 		catch (IOException e) { System.err.println("Erreur : " + e.getMessage()); } 
 	}
 
--- CENOFM/IHM/FrameGraphique.java
+++ CENOFM/IHM/FrameGraphique.java
@@ -1,62 +1,90 @@
 package CENOFM.IHM;
 
 import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.graph.Edge;
 import org.graphstream.graph.Node;
 import org.graphstream.ui.view.Viewer;
 
 import java.util.List;
+
 import CENOFM.metier.Noeud;
 
 public class FrameGraphique
 {
 	private List<String> couleurs;
-	public FrameGraphique(int nbArcs, List<Noeud> clients)
+	public FrameGraphique(List<List<Noeud>> solution)
 	{
 		this.couleurs = List.of("#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#800000", "#008000", "#000080", "#808000");
-		int nbClients = clients.size();
-
-		Node[] graphNodes = new Node[nbClients];
-		Node node;
 
 		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
 
 		SingleGraph graph = new SingleGraph("CENOFM");
 		graph.setAttribute("ui.stylesheet", "node { shape:circle; fill-color:white; stroke-mode:plain; stroke-width:1; padding:5; stroke-color:black; text-mode:normal; text-background-color:white; text-alignment:center; text-size:16; size-mode:fit; }");
 
-		// Création des noeuds
-		for (Noeud client : clients)
-		{
-			if (client.getId() == 1)
-				node = graph.addNode("Dépôt");
-			else
-				node = graph.addNode("" + (client.getId() - 1));
-
-			node.setAttribute("ui.label", node.getId());
-			graphNodes[client.getId() - 1] = node;
-		}
-
-		// Création des arcs
-		for (int i = 0 ; i < graph.getNodeCount() ; i++)
-		{
-			int j = i + 1;
-
-			if (j >= graph.getNodeCount())
-				j = 0;
-			String edgeId = "Arc " + i + "-" + j;
-			if (!graph.getNode(graphNodes[i].getId()).hasEdgeBetween(graphNodes[j].getId()))
-			{
-				graph.addEdge(edgeId, graphNodes[i].getId(), graphNodes[j].getId(), true);
-				// this.colorerArc(graphNodes[i], graphNodes[j], this.couleurs.get(i % this.couleurs.size()));
-			}
-		}
+		this.creationNodes(graph, solution);
+		this.creationArcs (graph, solution);
 
 		Viewer viewer = graph.display();
 		viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.HIDE_ONLY);
 	}
 
+	private void creationNodes(SingleGraph graph, List<List<Noeud>> solution)
+	{
+		Node node;
+
+		// Création des noeuds
+		for (List<Noeud> clientList : solution)
+		{
+			for (Noeud client : clientList)
+			{
+				if (client.getId() == 0)
+					if (graph.getNode("0") == null)
+						node = graph.addNode("" + client.getId());
+					else
+						node = null;
+				else
+					node = graph.addNode("" + client.getId());
+
+				if (node != null)
+				{
+					if (client.getId() == 0)
+					{
+						node.setAttribute("ui.label", "Dépôt");
+						this.colorerNode(node, "#b7b7b7");
+					}
+					else
+						node.setAttribute("ui.label", node.getId());
+				}
+			}
+		}
+	}
+
+	private void creationArcs(SingleGraph graph, List<List<Noeud>> solution)
+	{
+		// Création des arcs
+		for (int vehicule = 0 ; vehicule < solution.size(); vehicule++)
+		{
+			List<Noeud> clientList = solution.get(vehicule);
+
+			for (int i = 0; i < clientList.size(); i++)
+			{
+				Noeud client1 = clientList.get(i);
+				Noeud client2 = (i + 1 < clientList.size()) ? clientList.get(i + 1) : clientList.get(0);
+
+				if (!graph.getNode("" + client1.getId()).hasEdgeBetween("" + client2.getId()))
+				{
+					String edgeId = "Arc " + client1.getId() + "-" + client2.getId();
+					Edge edge = graph.addEdge(edgeId, graph.getNode("" + client1.getId()).getId(), graph.getNode("" + client2.getId()).getId(), true);
+					this.colorerArc(graph.getNode("" + client1.getId()), graph.getNode("" + client2.getId()), this.couleurs.get(vehicule % this.couleurs.size()));
+					edge.setAttribute("ui.label", String.format("%.2f", client1.distance(client2)));
+				}
+			}
+		}
+	}
+
 	private void colorerNode(Node node, String color)
 	{
-		node.addAttribute("ui.style", "fill-color:" + color + ";");
+		node.addAttribute("ui.style", "fill-color:" + color + "; stroke-color:" + color + ";");
 	}
 
 	private void colorerArc(Node n1, Node n2, String color)
