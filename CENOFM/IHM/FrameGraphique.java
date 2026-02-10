6fcf69b442cf6f6d484523cd690ab87fe0bd6000
Thibault Mangeant <166180018+ThibaultMangeant@users.noreply.github.com>
2026-02-10 14:51:05 +0100

    Changement style graphe + methode couleur

--- CENOFM/IHM/FrameGraphique.java
+++ CENOFM/IHM/FrameGraphique.java
@@ -11,16 +11,15 @@ public class FrameGraphique
 {
 	public FrameGraphique(int nbArcs, List<Noeud> clients)
 	{
-		System.out.println("Nombre de clients : " + clients.size());
 		int nbClients = clients.size();
 
-		Node node;
 		Node[] graphNodes = new Node[nbClients];
+		Node node;
 
 		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
 
 		SingleGraph graph = new SingleGraph("CENOFM");
-		graph.setAttribute("ui.stylesheet", "node { shape:circle; fill-color:white; stroke-mode:plain; stroke-width:2; padding:10; stroke-color:black; text-mode:normal; text-background-color:white; text-alignment:center; text-size:16; size-mode:fit; }");
+		graph.setAttribute("ui.stylesheet", "node { shape:circle; fill-color:white; stroke-mode:plain; stroke-width:1; padding:5; stroke-color:black; text-mode:normal; text-background-color:white; text-alignment:center; text-size:16; size-mode:fit; }");
 
 		for (Noeud client : clients)
 		{
@@ -30,7 +29,6 @@ public class FrameGraphique
 				node = graph.addNode("" + (client.getId() - 1));
 
 			node.setAttribute("ui.label", node.getId());
-			System.out.println("Création du noeud : " + node.getId());
 			graphNodes[client.getId() - 1] = node;
 		}
 
@@ -44,10 +42,24 @@ public class FrameGraphique
 			if (!graph.getNode(graphNodes[i].getId()).hasEdgeBetween(graphNodes[j].getId()))
 			{
 				graph.addEdge(edgeId, graphNodes[i].getId(), graphNodes[j].getId(), true);
+				// this.colorerArc(graphNodes[i], graphNodes[j], "red");
 			}
 		}
 
 		Viewer viewer = graph.display();
 		viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.HIDE_ONLY);
+	}
+
+	private void colorerNode(Node node, String color)
+	{
+		node.addAttribute("ui.style", "fill-color:" + color + ";");
+	}
+
+	private void colorerArc(Node n1, Node n2, String color)
+	{
+		if (n1.hasEdgeBetween(n2.getId()))
+		{
+			n1.getEdgeBetween(n2.getId()).addAttribute("ui.style", "fill-color:" + color + ";  size:3;");
+		}
 	}
 }
